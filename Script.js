f832099d95dfcd1ccc38f295adc2769bc35d6805
Rishant Dubey <85980300+Rishant9135@users.noreply.github.com>
2024-02-27 00:40:00 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+function openJava(){
+    window.location.href = './Java/Java.html';
+}
+
+function openJavaScript(){
+    window.location.href = './JavaScript/JavaScript.html';
+}
+function openDBMS(){
+    window.location.href = './DBMS/DBMS.html';
+}
+function openASP(){
+    window.location.href = './ASP.NET/ASP.html';
+}
+function openCProgramming(){
+    window.location.href = './C Programming/C_Questions.html';
+}
